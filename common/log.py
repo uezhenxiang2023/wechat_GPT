e38dd46160e4e98ecabf5b20d4cf08911b5aa3de
uezhenxiang2023 <uezhenxiang2023@gmail.com>
2025-05-14 11:43:39 +0800

    Enhance logging configuration; set console handler to INFO level, file handler to DEBUG level, and update logger level for improved log detail.

--- common/log.py
+++ common/log.py
@@ -10,6 +10,8 @@ def _reset_logger(log):
     log.handlers.clear()
     log.propagate = False
     console_handle = logging.StreamHandler(sys.stdout)
+    # 控制台 handler 设置为 INFO 级别
+    console_handle.setLevel(logging.INFO)
     console_handle.setFormatter(
         logging.Formatter(
             "[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s",
@@ -17,6 +19,8 @@ def _reset_logger(log):
         )
     )
     file_handle = logging.FileHandler("run.log", encoding="utf-8")
+    # 文件 handler 设置为 DEBUG 级别
+    file_handle.setLevel(logging.DEBUG)
     file_handle.setFormatter(
         logging.Formatter(
             "[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s",
@@ -30,7 +34,7 @@ def _reset_logger(log):
 def _get_logger():
     log = logging.getLogger("log")
     _reset_logger(log)
-    log.setLevel(logging.INFO)
+    log.setLevel(logging.DEBUG)
     return log
 
 
