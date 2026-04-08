8322f32c8536d6013e90d8c8cae390a0e75a7401
uezhenxiang2023 <142559986+uezhenxiang2023@users.noreply.github.com>
2026-04-08 16:56:54 +0800

    Update USER_IMAGE_CACHE expiration time to 5 minutes for improved caching efficiency

--- common/memory.py
+++ common/memory.py
@@ -1,6 +1,6 @@
 from common.expired_dict import ExpiredDict
 
-USER_IMAGE_CACHE = ExpiredDict(60 * 3)
+USER_IMAGE_CACHE = ExpiredDict(60 *5)
 USER_FILE_CACHE = ExpiredDict(60 * 10)
 USER_VIDEO_CACHE = ExpiredDict(60 * 10)
 
